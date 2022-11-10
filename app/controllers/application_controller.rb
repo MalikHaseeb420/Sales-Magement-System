e6b28a49ecbfbf374231f367781796a24776feae
Muhammad Haseeb Malik <haseeb.nazeer@devsinc.com>
2022-11-10 13:03:17 +0500

    added devise new user registration - added user registration using devise - edit navber for current user (dropdown) - added gender enum in users models - permit some extra attributes for devise registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[name gender email password
+                                        profile_image user_role_id])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[name gender email
+                                        profile_image user_role_id])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,:confirmable, :lockable
+  enum gender:{male:0,female:1,other:2}
+  has_one_attached :profile_image
 end
