6e64d106319a4cd5660b394970ee0354b38ecd4a
Muhammad Haseeb Malik <haseeb.nazeer@devsinc.com>
2022-11-21 17:39:45 +0500

    testing reflog

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
+  def do_something
+    puts '1232'
+  end
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: %i[name gender email password
